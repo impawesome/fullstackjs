3e6fe0454ba04e608d6f9e29787ec2f359ecf721
pawbibo <jesus.cabelin@bibo.com.ph>
2017-07-17 18:17:37 +0800

    state and style

--- learnyoureact/components/todo.js
+++ learnyoureact/components/todo.js
@@ -1,17 +1,38 @@
 import React from 'react';
 
 class Todo extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      checked: false
+    }
+  }
+
+  handleChange(event){
+    this.setState(event.target.checked)
+  }
+
   render() {
     return (
       <tr>
-        <td style={{border: '1px solid black'}}>{ this.props.title }</td>
-        <td style={{border: '1px solid black'}}>{ this.props.children }</td>
+        <td style={style.tableContent}>
+            <input type="checkbox" checked={this.state.checked} onChange={this.handleChange.bind(this)}/>
+        </td>
+        <td style={style.tableContent}>{ this.props.title }</td>
+        <td style={style.tableContent}>{ this.props.children }</td>
       </tr>
     );
   }
 }
-  Todo.propTypes = {
+
+Todo.propTypes = {
     title: React.PropTypes.string.isRequired
-  }
+}
+
+const style = {
+    tableContent: {
+        border: '1px solid black'
+    }
+}
 
 export default Todo
